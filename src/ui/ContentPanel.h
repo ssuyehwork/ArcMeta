27c49f3f15a2398fcdd1b19adc83cc73aa0cc185
ssuyehwork <166781457+ssuyehwork@users.noreply.github.com>
2026-07-12 02:56:52 +0000

    refactor: 彻底解决 ToolTipOverlay 在 ScanDialog 中悬浮不隐藏与延迟显示问题，修复启动空指针崩溃

--- src/ui/ContentPanel.h
+++ src/ui/ContentPanel.h
@@ -439,6 +439,7 @@ public:
     QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const override;
     bool eventFilter(QObject* obj, QEvent* event) override;
     bool editorEvent(QEvent* event, QAbstractItemModel* model, const QStyleOptionViewItem& option, const QModelIndex& index) override;
+    bool helpEvent(QHelpEvent* event, QAbstractItemView* view, const QStyleOptionViewItem& option, const QModelIndex& index) override;
 
     QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
     void setEditorData(QWidget* editor, const QModelIndex& index) const override;
