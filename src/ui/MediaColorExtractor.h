111cd9d316169d173a75b378c3a540dc8d07d8ce
ssuyeh <ssuyehwork@gmail.com>
2026-08-09 22:34:12 +0700

    Sync: 2026-08-09 22:34:12

--- src/ui/MediaColorExtractor.h
+++ src/ui/MediaColorExtractor.h
@@ -5,12 +5,11 @@
 #include <QString>
 #include <QVector>
 #include <QPair>
+#include "FormatDecoders.h"
+#include "ImageDecoderFacade.h"
+#include "ColorAlgorithmEngine.h"
 
 namespace ArcMeta {
-
-struct LabColor {
-    double l, a, b;
-};
 
 class MediaColorExtractor {
 public:
@@ -19,17 +18,33 @@ public:
     static QColor getExtensionColor(const QString& ext);
     static QColor quantizeColor(const QColor& color);
     
-    static LabColor rgbToLab(const QColor& color);
-    static double calculateDeltaE(const QColor& c1, const QColor& c2);
+    static LabColor rgbToLab(const QColor& color) {
+        auto res = ColorAlgorithmEngine::rgbToLab(color);
+        return {res.l, res.a, res.b};
+    }
+    
+    static double calculateDeltaE(const QColor& c1, const QColor& c2) {
+        return ColorAlgorithmEngine::calculateDeltaE(c1, c2);
+    }
+    
     static QVector<QPair<QColor, float>> extractPalette(const QString& targetFile);
     static QColor extractDominantColor(const QString& targetFile);
-    static QImage extractEmbeddedPsdThumbnail(const QString& path);
-    static QImage extractEmbeddedAiPreview(const QString& path, int targetSize = 512);
-    static QImage extractEmbeddedEpsPreview(const QString& path, int targetSize = 512);
-    static QImage renderPdfAiFirstPage(const QString& filePath, int targetSize = 512);
-    static QImage renderWithGhostscript(const QString& filePath, int targetSize = 512);
-private:
-    static QString findGhostscriptExecutable();
+    
+    static QImage extractEmbeddedPsdThumbnail(const QString& path) {
+        return FormatDecoders::extractPsdHeaderThumbnail(path);
+    }
+    
+    static QImage extractEmbeddedAiPreview(const QString& path, int targetSize = 512) {
+        return FormatDecoders::extractAiPreview(path, targetSize);
+    }
+    
+    static QImage extractEmbeddedEpsPreview(const QString& path, int targetSize = 512) {
+        return FormatDecoders::extractEpsPreview(path, targetSize);
+    }
+    
+    static QImage renderWithGhostscript(const QString& filePath, int targetSize = 512) {
+        return FormatDecoders::renderGhostscriptSafely(filePath, targetSize);
+    }
 };
 
 } // namespace ArcMeta
